35a2202f970c66fb66835e283b1fd811b643a292
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-03-30 19:26:06 +0530

    classnames util extended to handle undefined and boolean values

--- utils/classNames.ts
+++ utils/classNames.ts
@@ -1,3 +1,5 @@
-export default function classNames(...classes: string[]): string {
+export default function classNames(
+  ...classes: Array<string | boolean | undefined>
+): string {
   return classes.filter(Boolean).join(" ")
 }
