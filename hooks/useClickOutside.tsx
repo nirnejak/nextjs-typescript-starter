b1f5d89f39cb711362577c689c174a681e524da2
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-06-09 00:04:23 +0530

    useClickOutside hook fixed

--- hooks/useClickOutside.tsx
+++ hooks/useClickOutside.tsx
@@ -1,21 +1,29 @@
 import * as React from "react"
 
-const useClickOutside = (ref, handler) => {
+type Event = MouseEvent | TouchEvent
+
+const useClickOutside = <T extends HTMLElement = HTMLElement>(
+  ref: React.RefObject<T>,
+  handler: (event: Event) => void
+): void => {
   React.useEffect(() => {
-    const listener = (event) => {
-      if (!ref.current || ref.current.contains(event.target)) {
+    const listener = (event: Event): void => {
+      const el = ref?.current
+      if (el == null || el.contains(event?.target as Node)) {
         return
       }
 
-      handler(event)
+      handler(event) // Call the handler only if the click is outside of the element passed.
     }
 
-    document.addEventListener("click", listener)
+    document.addEventListener("mousedown", listener)
+    document.addEventListener("touchstart", listener)
 
     return () => {
-      document.removeEventListener("click", listener)
+      document.removeEventListener("mousedown", listener)
+      document.removeEventListener("touchstart", listener)
     }
-  }, [ref, handler])
+  }, [ref, handler]) // Reload only if ref or handler changes
 }
 
 export default useClickOutside
