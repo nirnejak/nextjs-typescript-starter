545c17ffe9892ef0442fd562c534736eb6ab0b16
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-05-09 19:56:18 +0530

    Home: text updated

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
 const Home: React.FC = () => {
   return (
     <main className="grid h-screen place-content-center">
-      <h1 className="text-center text-8xl font-bold text-zinc-300">
+      <h1 className="text-center text-xl font-medium text-zinc-300">
         Welcome to <a href="https://nextjs.org">Next.js!</a>
       </h1>
     </main>
