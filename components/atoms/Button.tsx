09156d6408235d9464881466969f4d7811897046
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-07-01 21:44:46 +0530

    Button: variant style updated, variance props issue fixed

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -7,16 +7,16 @@ import classNames from "@/utils/classNames"
 const buttonVariants = cva("rounded-md", {
   variants: {
     variant: {
-      primary: "bg-blue-500",
-      secondary: "bg-gray-500",
+      primary: "bg-white",
+      secondary: "bg-gray-400",
     },
     size: {
-      sm: "px-2 py-1",
-      md: "px-3 py-2",
-      lg: "px-4 py-3",
+      sm: "px-5 py-1.5 text-sm",
+      md: "px-6 py-2",
+      lg: "px-8 py-3 text-lg",
     },
     defaultVariants: {
-      variant: "primary",
+      variant: "secondary",
       size: "md",
     },
   },
@@ -28,14 +28,17 @@ export interface Props
   asChild?: boolean
 }
 
-const Button: React.FC<Props> = ({ children, className, ...props }) => {
+const Button: React.FC<Props> = ({
+  children,
+  className,
+  variant,
+  size,
+  ...props
+}) => {
   return (
     <button
       {...props}
-      className={classNames(
-        buttonVariants({ variant: "primary", size: "md" }),
-        className
-      )}
+      className={classNames(buttonVariants({ variant, size }), className)}
     >
       {children}
     </button>
