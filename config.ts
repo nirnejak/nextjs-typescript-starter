4b8bd4135052f8c7fa7883b6b0e59ccb95bd48fc
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-07-27 04:05:59 +0530

    config file added

---
+++ config.ts
@@ -0,0 +1,24 @@
+const config = {
+  baseUrl: "https://example.com",
+  author: "",
+  domainName: "my-app",
+  appName: "<Application Name>",
+  appDescription: "<Application Description>",
+  creator: "<Creator Name>",
+  authorName: "<Author Name>",
+  authorUrl: "<Author Name or Email>",
+  keywords: [
+    "Next.js",
+    "TailwindCSS",
+    "Framer Motion",
+    "TypeScript",
+    "Akar Icons",
+    "ESLint",
+    "Prettier",
+    "JavaScript",
+    "Postgres",
+  ],
+  twitterSite: "@site",
+  twitterCreator: "@creator",
+}
+export default config
