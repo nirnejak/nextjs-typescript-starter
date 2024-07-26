36d7e5b19b77b1bf457a54a8cbb85672a830f19c
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-07-27 04:15:13 +0530

    config: unused fields removed

--- config.ts
+++ config.ts
@@ -1,7 +1,5 @@
 const config = {
   baseUrl: "https://example.com",
-  author: "",
-  domainName: "my-app",
   appName: "<Application Name>",
   appDescription: "<Application Description>",
   creator: "<Creator Name>",
