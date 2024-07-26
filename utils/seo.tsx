7b944d03a0d8791dfa45c1034f1fa5a9ba419659
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-07-27 04:16:52 +0530

    seo: schema.org renderer added

--- utils/seo.tsx
+++ utils/seo.tsx
@@ -1,4 +1,4 @@
-import { type Metadata } from "next" // Don't include slash at the end
+import { type Metadata } from "next"
 
 import config from "@/config"
 
@@ -9,7 +9,7 @@ interface MetadataArgs {
   image?: string
 }
 
-const generateMetadata = ({
+export const generateMetadata = ({
   path,
   title,
   description,
@@ -76,4 +76,40 @@ const generateMetadata = ({
   return metadata
 }
 
-export default generateMetadata
+// INFO: Learn more at https://developers.google.com/search/docs/appearance/structured-data/intro-structured-data
+// INFO: Test at https://search.google.com/test/rich-results
+export const renderSchemaTags = (): React.ReactNode => {
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify({
+          "@context": "http://schema.org",
+          "@type": "SoftwareApplication", // INFO: Find your type here (SoftwareApp, Book...): https://developers.google.com/search/docs/appearance/structured-data/search-gallery
+          name: config.appName,
+          description: config.appDescription,
+          image: `${config.baseUrl}/icon.png`,
+          url: `${config.baseUrl}/`,
+          author: {
+            "@type": "Person",
+            name: config.authorName,
+          },
+          datePublished: "2023-08-01",
+          applicationCategory: "EducationalApplication",
+          aggregateRating: {
+            "@type": "AggregateRating",
+            ratingValue: "4.8",
+            ratingCount: "26",
+          },
+          offers: [
+            {
+              "@type": "Offer",
+              price: "9.00",
+              priceCurrency: "USD",
+            },
+          ],
+        }),
+      }}
+    ></script>
+  )
+}
