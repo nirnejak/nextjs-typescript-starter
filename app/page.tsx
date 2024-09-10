211d00fc4ed868133bf853f816181cc0790a1879
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-09-10 06:53:03 +0530

    Home: reveal animation added

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import * as React from "react"
 
 import { type Metadata } from "next"
 
+import * as motion from "framer-motion/client"
+
+import { BASE_TRANSITION } from "utils/animation"
 import { generateMetadata } from "utils/seo"
 
 export const metadata: Metadata = generateMetadata({
@@ -13,9 +16,14 @@ export const metadata: Metadata = generateMetadata({
 const Home: React.FC = () => {
   return (
     <main className="grid h-screen place-content-center">
-      <h1 className="text-5xl font-semibold tracking-tighter text-zinc-400">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+      <motion.h1
+        initial={{ translateY: 20, opacity: 0 }}
+        animate={{ translateY: 0, opacity: 1 }}
+        transition={{ delay: 0, ...BASE_TRANSITION }}
+        className="text-5xl font-semibold tracking-tighter text-zinc-400"
+      >
+        Welcome to my Next.js starter!
+      </motion.h1>
     </main>
   )
 }
