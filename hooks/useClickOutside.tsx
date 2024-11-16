981cced4e4bfcbffaaa468cdc3e5b335c1bae296
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-11-16 18:17:31 +0530

    useClickOutside: returning ref

--- hooks/useClickOutside.tsx
+++ hooks/useClickOutside.tsx
@@ -2,10 +2,11 @@ import * as React from "react"
 
 type Event = MouseEvent | TouchEvent
 
-const useClickOutside = <T extends HTMLElement = HTMLElement>(
-  ref: React.RefObject<T>,
+const useClickOutside = (
   handler: (event: Event) => void
-): void => {
+): React.RefObject<HTMLElement> => {
+  const ref = React.useRef<HTMLElement>(null)
+
   React.useEffect(() => {
     const listener = (event: Event): void => {
       const el = ref?.current
@@ -24,6 +25,8 @@ const useClickOutside = <T extends HTMLElement = HTMLElement>(
       document.removeEventListener("touchstart", listener)
     }
   }, [ref, handler]) // Reload only if ref or handler changes
+
+  return ref
 }
 
 export default useClickOutside
