8aa785afa065d9956cdc235a1666494133a77a0d
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-12-12 12:22:28 +0530

    H1 style updated

--- app/page.tsx
+++ app/page.tsx
@@ -17,10 +17,10 @@ const Home: React.FC = () => {
   return (
     <main className="grid h-screen place-content-center">
       <motion.h1
-        initial={{ translateY: 20, opacity: 0 }}
-        animate={{ translateY: 0, opacity: 1 }}
+        initial={{ translateY: 20, opacity: 0, filter: `blur(10px)` }}
+        animate={{ translateY: 0, opacity: 1, filter: "none" }}
         transition={{ delay: 0, ...BASE_TRANSITION }}
-        className="text-5xl font-semibold tracking-tighter text-zinc-800 dark:text-zinc-300"
+        className="text-5xl font-bold tracking-tighter text-zinc-800 dark:text-zinc-300"
       >
         Welcome to the Next.js starter!
       </motion.h1>
