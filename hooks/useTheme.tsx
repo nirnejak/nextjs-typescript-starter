396e6757e97611233237043e22f2a9a585a44095
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-23 15:19:04 +0530

    useTheme Hook updated

--- hooks/useTheme.tsx
+++ hooks/useTheme.tsx
@@ -1,17 +1,38 @@
 import React from "react"
 
-const useTheme = (): boolean => {
-  const [isDark, setIsDark] = React.useState(false)
+type THEME_OPTIONS = "system" | "light" | "dark"
+
+interface THEME_HOOK {
+  theme: THEME_OPTIONS
+  setTheme: React.Dispatch<React.SetStateAction<THEME_OPTIONS>>
+}
+
+const useTheme = (): THEME_HOOK => {
+  const [theme, setTheme] = React.useState<THEME_OPTIONS>("system")
 
   React.useEffect(() => {
-    window
-      .matchMedia("(prefers-color-scheme: dark)")
-      .addEventListener("change", (event) => {
-        setIsDark(event.matches)
-      })
+    setTheme(() => {
+      const savedTheme = localStorage.getItem("theme") as THEME_OPTIONS | null
+      return savedTheme ?? "system"
+    })
   }, [])
 
-  return isDark
+  React.useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)")
+
+    const handleSystemThemeChange = (e: MediaQueryListEvent): void => {
+      if (theme === "system") {
+        document.documentElement.classList.toggle("dark", e.matches)
+      }
+    }
+
+    mediaQuery.addEventListener("change", handleSystemThemeChange)
+    return () => {
+      mediaQuery.removeEventListener("change", handleSystemThemeChange)
+    }
+  }, [theme])
+
+  return { theme, setTheme }
 }
 
 export default useTheme
