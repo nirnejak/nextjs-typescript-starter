ac6da22210aac3d0459c06c8fbec6ee9cb40e7c6
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-04-10 07:03:37 +0530

    MDX Loader Setup

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next"
+import createMDX from "@next/mdx"
 
 const nextConfig: NextConfig = {
   trailingSlash: true,
   reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 }
 
-export default nextConfig
+const withMDX = createMDX({
+  options: {},
+})
+
+export default withMDX(nextConfig)
