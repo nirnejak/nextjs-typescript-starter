ae06fcbb06f02125a6388b5616b05ca6b45b5d4b
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-06-28 03:01:19 +0530

    animation util updated

--- utils/animation.ts
+++ utils/animation.ts
@@ -1,6 +1,8 @@
+import { AnimationGeneratorName } from "motion"
+
 export const BASE_TRANSITION = {
   duration: 0.15,
-  type: "spring",
+  type: "spring" as AnimationGeneratorName,
   stiffness: 200,
   damping: 15,
 }
